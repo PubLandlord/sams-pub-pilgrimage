081d23862a111bbec603abf885981c069ccb89fc
PubLandlord <ingld001@gmail.com>
2026-07-28 23:43:01 +0100

    Version 0.2 - Book navigation

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Sam's Pub Pilgrimage loaded.");
+const pages=document.querySelectorAll(".page");
+
+let currentPage=0;
+
+function showPage(index){
+
+pages.forEach(page=>page.classList.remove("active"));
+
+pages[index].classList.add("active");
+
+}
+
+document.getElementById("next").addEventListener("click",()=>{
+
+if(currentPage<pages.length-1){
+
+currentPage++;
+
+showPage(currentPage);
+
+}
+
+});
+
+document.getElementById("previous").addEventListener("click",()=>{
+
+if(currentPage>0){
+
+currentPage--;
+
+showPage(currentPage);
+
+}
+
+});
+
+document.getElementById("openBook").addEventListener("click",()=>{
+
+currentPage=1;
+
+showPage(currentPage);
+
+});
